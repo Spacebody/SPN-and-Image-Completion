5b3c081d4cbef1192dea387d179b859732b5249d
Spacebody <510662916@qq.com>
2018-11-22 00:58:31 +0800

    Update sum_node

--- Implementation/codes/spn/sum_node.hpp
+++ Implementation/codes/spn/sum_node.hpp
@@ -3,10 +3,29 @@
 
 #include "node.hpp"
 #include <map>
+#include "../common/parameter.hpp"
+
 class SumNode: public Node
 {
     public:
-        
+        std::map<std::string, Node> children;
+        std::map<std::string, double> child_cnts;
+        double cnt;
+
+        SumNode()
+        {
+            this->children.empty();
+            this->child_cnts.empty();
+            this->cnt = Parameter::smooth_sum_cnt;
+        }
+        ~SumNode() {}
+
+        void eval();
+        void pass_derivative();
+        double get_child_cnt(std::string di);
+        void set_child_cnt(std::string di, double cnt);
+        void add_child_only(std::string, double cnt, Node n);
+        void remove_child_only(std::string deomp_idx, double cnt);
 };
 
 #endif
