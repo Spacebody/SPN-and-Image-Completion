3fc3acfbdef92361f932a0890ba01eb2490fe078
Spacebody <510662916@qq.com>
2019-03-10 23:46:14 +0800

    Update Tests

---
+++ Implementation/tests/evaluation/test_image_completiom.cpp
@@ -0,0 +1,8 @@
+#include "../../codes/evaluation/image_completion.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
---
+++ Implementation/tests/evaluation/test_run.cpp
@@ -0,0 +1,8 @@
+#include "../../codes/evaluation/run.hpp"
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+
+    return 0;
+}
