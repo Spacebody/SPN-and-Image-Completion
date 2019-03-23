3ec16a737c68970da149fc4bb895687b8125a718
Spacebody <510662916@qq.com>
2019-03-23 20:18:44 +0800

    Update src

--- Implementation/codes/evaluation/run_main.cpp
+++ Implementation/codes/evaluation/run_main.cpp
@@ -1,11 +1,13 @@
 #include "run.hpp"
 #include <mpi.h>
 #include "../common/my_mpi.hpp"
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     MPI_Init(NULL, NULL); // initialize MPI environment
     MPI_Comm_rank(MPI_COMM_WORLD, &MyMPI::rank);  // set rank
+    std::cout << "My rank: " << MyMPI::rank << std::endl;
     Run run;
     run.proc(argc, argv);
     MPI_Finalize();  // clean MPI environment
