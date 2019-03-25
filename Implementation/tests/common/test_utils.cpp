6437ff49a99f1e790028f9f11045bd6141333981
Spacebody <510662916@qq.com>
2019-03-25 13:49:29 +0800

    Update test

--- Implementation/tests/common/test_utils.cpp
+++ Implementation/tests/common/test_utils.cpp
@@ -7,9 +7,8 @@ int main(int argc, char *argv[])
     Utils utils;
     Instance inst;
     std::cout << "Time interval: " << Utils::get_int_val(inst, 1.0) << std::endl;
-    std::string log_msg = "Test";
-    Utils::log_time(log_msg);
-    Utils::log_time_ms(log_msg);
+    Utils::log_time("Test");
+    Utils::log_time_ms("Test");
     std::cout << Utils::get_prefix() << std::endl;
     return 0;
 }
