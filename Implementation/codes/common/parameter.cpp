d9589cc11f513376d73037daaebbbf4117d814e3
Spacebody <510662916@qq.com>
2019-04-23 03:29:50 +0800

    Update src

--- Implementation/codes/common/parameter.cpp
+++ Implementation/codes/common/parameter.cpp
@@ -2,17 +2,17 @@
 
 int Parameter::max_iter = 30;
 double Parameter::threshold_LLHChg = 0.1;
-int Parameter::batch_size = 10;  //50
+int Parameter::batch_size = 50;  //50
 double Parameter::sparse_prior = 1;
 
 int Parameter::num_sum_per_region = 20;
-int Parameter::input_dim1 = 100;
+int Parameter::input_dim1 = 64;
 int Parameter::input_dim2 = 64;
 int Parameter::base_resolution = 4;
 double Parameter::smooth_sum_cnt = 0.01;
 int Parameter::num_components_per_var = 4;
 
-int Parameter::max_test_size = 40; // 50
+int Parameter::max_test_size = 50; // 50
 std::string Parameter::domain = "";
-int Parameter::num_slave_per_class = 1;  //50
+int Parameter::num_slave_per_class = 2;  //50
 int Parameter::num_slave_grp = 2;
